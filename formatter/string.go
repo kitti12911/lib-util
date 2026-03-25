7e58bf33daa3912651fd185bf336226a0f461a6a
Kitti Srijantanakul <kitti12911@gmail.com>
2026-03-25 15:45:58 +0700

    feat: add string formatter package

---
+++ formatter/string.go
@@ -0,0 +1,22 @@
+package formatter
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+func ToJSON(v any, indent bool) (string, error) {
+	var jsonText []byte
+	var err error
+	if indent {
+		jsonText, err = json.MarshalIndent(v, "", "  ")
+	} else {
+		jsonText, err = json.Marshal(v)
+	}
+
+	if err != nil {
+		return "", fmt.Errorf("formatter: to json: %w", err)
+	}
+
+	return string(jsonText), nil
+}
